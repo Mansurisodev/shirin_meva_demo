04c323f579b86c0e425a7d57f4e06e2e75d33857
Mansuriosdev <mansuriosdevm1@gmail.com>
2023-01-27 19:06:17 +0500

    //new version 2023.01.27 18:59

---
+++ assets/utils/colors.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'dart:ui';
+
+class AppColors {
+  static final Color textColor = const Color(0xFFcc7c5);
+  static final Color mainColor = const Color(0xFF89dad0);
+  static final Color iconColor1 = const Color(0xFFffd28d);
+  static final Color iconColor2 = const Color(0xFFfcab88);
+  static final Color paraColor = const Color(0xFF8f837f);
+  static final Color buttomBacgroundColor = const Color(0xFFf7f6f4);
+  static final Color signColor = const Color(0xFFa9a229f);
+  static final Color titleColor = const Color(0xFF5c524f);
+  static final Color mainBlackColor = const Color(0xFF332d2b);
+  static final Color yellowColor = const Color(0xFFffd379);
+
+  static final Color greyColor1 = const Color(0xFFB8B8B8);
+  static final Color mainWhite = const Color(0xFFFFFFFFF);
+  static final Color darkBigText = const Color(0xFF1D1D1D);
+  static final Color mainGreenColor = const Color(0xFF8FDA38);
+  static final Color searchCOlor = const Color(0xFFF7F7F7);
+
+  static final Color lateGreenColor = const Color(0xFFA8EE4A);
+
+
+
+}
+
+class BanneColor {
+  List<Widget> bannerColors = [
+  ];
+  static final Color oneBannerColor = const Color(0xFFF7F7F7);
+  static final Color twoBannerColor = const Color(0xFFF7F7F7);
+  static final Color threBannerColor = const Color(0xFFF7F7F7);
+  static final Color fiveBannerColor = const Color(0xFFF7F7F7);
+  static final Color whiteSearchButton = const Color(0xFFF7F7F7);
+  static final Color mainGreyWhiteColor = const Color(0xFF1E1E1E);
+
+
+}
